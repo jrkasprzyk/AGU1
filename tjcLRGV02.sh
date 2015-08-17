be212ddf3e13b15cc16d9c57540ff178e192d40f
Tim Clarkin <tjclarkin@gmail.com>
2015-08-17 16:43:54 -0600

    edited line about MOEA

--- tjcLRGV02.sh
+++ tjcLRGV02.sh
@@ -13,8 +13,7 @@ do
 # Select control file to run 
 FILEBASE="AllDecAll$i" 
 
-# The command shown below is what you need to complete a single seed, with Seed 1 shown here.
-# You can add additional commands by enclosing them in "()" and separating by "&".  I ran a test and found that running commands in this way yeilded the same results as separate scripts (for multiple seeds).
+# Command line for Borg MOEA
 	
 (./borg.exe -v 8 -o 6 -c 4 -C E -R ${FILEBASE}_runtime_s1.txt \
 -F 5000 -f ${FILEBASE}_s1.txt -l 0,0,0,0.1,0,0,0,0 -u 1,1,1,0.4,3,3,3,3 \
@@ -24,7 +23,7 @@ FILEBASE="AllDecAll$i"
 -c combined -r 5000) &
 
 #check to see which have begun
-echo scenario $i started
+echo scenario $i has begun
 
 done
 
